2bc92878a618aedd363027bf8c9c1fd542c08970
ctkim <nkdevil@naver.com>
2023-04-27 10:01:24 +0900

    router 및 url 변경
     - histroy mode 되돌림 및 baseurl 변경
     - github pages를 gthub action으로 배포하려공

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: './',
-  build: {
-    outDir: './docs'
-  }
+  base: '/myEZtools/',
 })
